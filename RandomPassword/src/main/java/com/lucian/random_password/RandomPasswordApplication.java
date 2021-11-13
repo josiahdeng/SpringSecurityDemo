5805b02e1c6fcd262feafeac03f0083a92be97f5
denglingxiang <17607003651@163.com>
2021-11-13 20:01:29 +0800

    随机密码模式🤣

---
+++ RandomPassword/src/main/java/com/lucian/random_password/RandomPasswordApplication.java
@@ -0,0 +1,13 @@
+package com.lucian.random_password;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomPasswordApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RandomPasswordApplication.class, args);
+    }
+
+}
